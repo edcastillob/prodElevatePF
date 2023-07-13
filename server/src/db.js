d60786c2d8edc45a75a44c9e72f3f7b6540b0435
edcastillob <edwar.castillo@gmail.com>
2023-07-13 18:42:14 -0400

    Name DB

--- server/src/db.js
+++ server/src/db.js
@@ -4,10 +4,11 @@ const { Sequelize } = require("sequelize");
 
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  
   logging: false, 
   native: false, 
 });
