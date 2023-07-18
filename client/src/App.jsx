99f5bcae619bf197f5d14783d4e941c9fd471acd
edcastillob <edwar.castillo@gmail.com>
2023-07-17 20:16:51 -0400

    Resolver conflicto de fusión en client/src/App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
+<<<<<<< Updated upstream
+=======
+import { Routes, Route } from 'react-router-dom';
+// import { UploadImg } from './components/Product/uploadImg/UploadImg'
+// import { Provider } from './components/Product/provider/Provider';
+// import { Category } from './components/Product/category/Category';
+import { Product } from './components/Product/createProduct/Product';
+import { Home } from './components/Home/Home';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <>
       <div>
+<<<<<<< Updated upstream
         <h1>Desarrollo prodElevate</h1>
+=======
+        <Product />
+      <Routes>
+          
+          {/* <Route exact path="/" element={ }/> */}
+          <Route exact path="/" element={<Home />}/>
+        
+      </Routes>
+
+>>>>>>> Stashed changes
       </div>
      
     </>
