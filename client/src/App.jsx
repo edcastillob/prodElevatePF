77638bde24b2ead00dc2261297683de761931602
edcastillob <edwar.castillo@gmail.com>
2023-07-18 18:29:10 -0400

    Landing, renderizado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,28 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 import { NavBar } from './components/NavBar/NavBar';
-import { Cart } from './components/Cart/Cart'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { ProductDetail } from './components/Product/productDetail/productDetail';
-
+import { Landing } from './components/Landing/Landing';
 
 function App() {
+  const location = useLocation();
+  const [showNavBar, setShowNavBar] = useState(true);
+
+  useEffect(() => {    
+    setShowNavBar(location.pathname !== '/');
+  }, [location]);
+
   return (
     <>
-      <NavBar/>        
-      <div>         
+      {showNavBar && <NavBar />}
+      <div>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Landing />} />
+          <Route exact path="/home" element={<Home />} />
           <Route path="/detail/:id" element={<ProductDetail />} />
         </Routes>
-        <Cart/> 
       </div>
     </>
   );
