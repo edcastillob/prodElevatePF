c5e17fff46f1428ed76073413b8b8b08f315103d
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-07-18 15:42:56 -0300

    NavBar, SearchBar, Landing and Cart Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
+import './App.css';
+import { NavBar } from './components/NavBar/NavBar';
+import { Cart } from './components/Cart/Cart'
+
+
+
+
 
 function App() {
   return (
     <>
+      <NavBar/>
       <div>
-        <h1>Desarrollo prodElevate</h1>
+        {/* <h1>Desarrollo prodElevate</h1> */}
+        <Cart/>
       </div>
-     
     </>
   )
 }
 
 export default App
-// Prueba de branch
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-// import store from "./redux/store/store.js";
-import  App  from "./App.jsx";
+// import { Provider } from "react-redux";
+// import store from "./redux/store";
+import App from "./App.jsx";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  // </Provider>
+  // {/* </Provider> */}
 );
