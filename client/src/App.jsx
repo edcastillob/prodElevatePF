8bbf65cc26377a83b083b29040cdbdf532746165
edcastillob <edwar.castillo@gmail.com>
2023-07-18 17:07:27 -0400

    Renderizando home y detail

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { Home } from './components/Home/Home';
 import { ProductDetail } from './components/Product/productDetail/productDetail';
+// import { Login } from './components/users/login/Login'
+// import { CreateUser } from './components/users/createUser/CreateUser';
 
 function App() {
   return (
     <>
       <div>        
-        <h1>Desarrollo prodElevate</h1>        
+        <h1>Desarrollo prodElevate</h1>            
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/detail/:id" element={<ProductDetail />} />
