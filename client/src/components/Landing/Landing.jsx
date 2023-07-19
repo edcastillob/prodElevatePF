9a76964bcb0d09262d5427c7b63b3862fafd4b07
edcastillob <edwar.castillo@gmail.com>
2023-07-18 21:39:59 -0400

    Logica de entrada en landing

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -7,11 +7,12 @@ export const Landing = () => {
         <div className="col-md-6 offset-md-3 text-center">
           <h1 className="display-4">Welcome to Our Website</h1>
           <p className="lead">
-          prodElevate will be a product administration and management system for online sales.
-The main goal is to provide users with a platform where they can create,
-manage and sell products efficiently. The system will have functionalities
-such as authentication and authorization, creation of products, categories, shopping cart,
-payment gateway, administration dashboard and notifications.
+            prodElevate will be a product administration and management system
+            for online sales. The main goal is to provide users with a platform
+            where they can create, manage and sell products efficiently. The
+            system will have functionalities such as authentication and
+            authorization, creation of products, categories, shopping cart,
+            payment gateway, administration dashboard and notifications.
           </p>
           <NavLink to="/home" className="btn btn-primary">
             Get Started
