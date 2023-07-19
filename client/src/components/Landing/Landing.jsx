323af7465716ff26eb11effa002f9d1331605aea
edcastillob <edwar.castillo@gmail.com>
2023-07-19 13:15:27 -0400

    LandingP

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -48,23 +48,7 @@ export const Landing = () => {
                 </div>
             </div>
 
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 offset-md-3 text-center">
-          <h1 className="display-4">Welcome to Our Website</h1>
-          <p className="lead">
-            prodElevate will be a product administration and management system
-            for online sales. The main goal is to provide users with a platform
-            where they can create, manage and sell products efficiently. The
-            system will have functionalities such as authentication and
-            authorization, creation of products, categories, shopping cart,
-            payment gateway, administration dashboard and notifications.
-          </p>
-          <NavLink to="/home" className="btn btn-primary">
-            Get Started
-          </NavLink>
-
-        </div>
-      
+    </div>
   );
 };
+
