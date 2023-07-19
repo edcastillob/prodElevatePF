95943ff41cd556f2e28484c41d1c407ed0a5ab7b
edcastillob <edwar.castillo@gmail.com>
2023-07-19 11:21:38 -0400

    Renderizado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,10 @@ import { NavBar } from "./components/NavBar/NavBar";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Home } from "./components/Home/Home";
 import { ProductDetail } from "./components/Product/productDetail/productDetail";
-import { Product } from "./components/Product/createProduct/Product";
-// import { Landing } from "./components/Landing/Landing";
+// import { Product } from "./components/Product/createProduct/Product";
+// import { Category } from "./components/Product/category/Category";
+// import { Provider } from "./components/Product/provider/Provider";
+import { Landing } from "./components/Landing/Landing";
 
 function App() {
   const location = useLocation();
@@ -20,8 +22,8 @@ function App() {
       {showNavBar && <NavBar />}
       <div>
         <Routes>
-          {/* <Route exact path="/" element={<Landing />} /> */}
-          <Route exact path="/" element={<Product />} />
+          <Route exact path="/" element={<Landing />} />
+          {/* <Route exact path="/" element={<Provider />} /> */}
           <Route exact path="/home" element={<Home />} />
           <Route path="/detail/:id" element={<ProductDetail />} />
         </Routes>
