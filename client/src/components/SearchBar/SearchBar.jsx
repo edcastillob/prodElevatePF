c0c2cb00457e1c605d6ab618c8e3dff5bad434e3
luisnavedag <luisjosenavedagarcia@gmail.com>
2023-07-19 11:16:48 -0300

    fix/SearchBar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import styles from "./SearchBar.module.css";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { getProductName } from "../../redux/actions/actions";
 
 
 
 export const SearchBar = () => {
-
+    const dispatch = useDispatch();
+    const [name, setName] = useState("");
+  
+    const handleChange = ({ target }) => {
+      const { value } = target;  
+      setName(value);
+    };
+  
+    const handleClick = (event) => {
+      event.preventDefault();
+      dispatch(getProductName(name));
+      setName("");
+    };
     return(
         <div className={styles.divSearchBar}>
             <input 
             type="text" 
+            name="name"
+            value={name}
+            autoComplete="off"
             className={styles.searchInput}
-            placeholder="Search product or category" />
-            <button><ion-icon name="search"></ion-icon></button>
+            placeholder="Search product or category"
+            onChange={handleChange} />
+            <button onClick={handleClick}><ion-icon name="search"></ion-icon></button>
         </div>
     )
 }
