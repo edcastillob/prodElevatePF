2bdd681672bfa09dc5b1f14072f4059f3b0fbcc0
yonathanbaez25 <yacevedo170@gmail.com>
2023-07-28 10:47:14 -0400

    Index

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const { conn } = require("./src/db");
 const PORT = 3001;
 
 conn
-  .sync({ force: false })
+  .sync({ force: true })
   .then(() => {
     server.listen(PORT, () => {
       console.log(`prodElevate Server listening in port ${PORT}...`);
