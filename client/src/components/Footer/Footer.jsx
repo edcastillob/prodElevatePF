244efabdb93d5ee3cbf4eb2c06d40252489f00cc
taio45678 <taiogamer@gmail.com>
2023-08-03 16:32:51 -0300

    Cambios completos

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -13,10 +13,10 @@ export const Footer = ({ currentLanguage }) => {
             <p>© {new Date().getFullYear()}{t("footer.all-rights-reserved", { lng: currentLanguage})}</p>
         </div>
         <div className={styles.divRight}>
-            <Link to='/about' style={{textDecoration: "none"}}><p>{t("footer.about-us", { lng: currentLanguage })}</p></Link>
-            <Link to='/privacy_policy' style={{textDecoration: "none"}}><p>{t("footer.privacy-policy", {lng: currentLanguage})}</p></Link>
-            <Link to='/terms_&_conditions' style={{textDecoration: "none"}}><p>{t("footer.terms-and-conditions", {lng: currentLanguage})}</p></Link>
-            <Link to='/contact' style={{textDecoration: "none"}}><p>{t("footer.contact-us", {lng: currentLanguage})}</p></Link>
+            <Link to='/about' style={{textDecoration: "none"}}><p><Info /> About Us</p></Link>
+            <Link to='/privacy_policy' style={{textDecoration: "none"}}><p><Policy /> Privacy Policy</p></Link>
+            <Link to='/terms_&_conditions' style={{textDecoration: "none"}}><p><Gavel /> Terms & Conditions</p></Link>
+            <Link to='/contact' style={{textDecoration: "none"}}><p><ContactMail /> Contact Us</p></Link>
         </div>
     </footer>
   );
