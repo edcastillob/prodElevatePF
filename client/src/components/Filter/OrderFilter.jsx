dd16ea8bb4fb6ebc8e40ccc21637bb1a6da844ae
Luis Naveda <luisjosenavedagarcia@gmail.com>
2023-08-05 15:05:03 -0300

    Role styles in settings, sweet alert in Cart, some details in Home and new traductions in components (#54)
    * mini changes in CreateProduct
    
    * Provider update
    
    * saving Provider, Configuration, Category, Product
    
    * New changes before git pull
    
    * Configuration, ProvidersAll
    
    * Changes before pull
    
    * Changes before merge
    
    * Settings
    
    * Forms in settings
    
    * Settings in Dashboard, fixing SettingsProduct component
    
    * Changes in Dashboard, Product
    
    * fix/Card container, Create user alerts, dashboard settings, navbar dropdown with dasboard in Local user
    
    * Add Fav button in NavBar
    
    * Fav button change and Login Form size
    
    * Add/Confirm Alerts in Delete
    
    * Edit Product, Edit Category, Edit Provider and Edit User
    
    * fixing errors
    
    * changes before merge
    
    * Thankyopage styles
    
    * changes to deploy
    
    * Instalación de React-i18next, cambios en el Footer, Navbar y Landing
    
    * Add Lng in NavBar, Login and Navbar Modified
    
    * fix/cardProduct details
    
    * changes before merge
    
    * changes before pull
    
    * changes before merge
    
    * Add role styles, sweet alert for Cart, and details in Home

--- client/src/components/Filter/OrderFilter.jsx
+++ client/src/components/Filter/OrderFilter.jsx
@@ -1,27 +1,31 @@
 import styles from "./OrderFilter.module.css";
+import { useTranslation } from 'react-i18next';
 
 const OrderFilter = ({
   handlePriceHigher,
   handlePriceLower,
   handleSortName,
   handleAllProdutcs,
+  currentLanguage
 }) => {
+  const { t } = useTranslation('global');
+
   return (
     <>
       <div className={styles.container}>
-        <h2 className={styles.orderTitle}>Order</h2>
+        <h2 className={styles.orderTitle}>{t("order-filter.order", { lng: currentLanguage })}</h2>
         <div className={styles.orderFlex}>
           <button className={styles.btns} onClick={handleAllProdutcs}>
-            All Products
+          {t("order-filter.all-products", { lng: currentLanguage })}
           </button>
           <button className={styles.btns} onClick={handleSortName}>
-            Name
+          {t("order-filter.name", { lng: currentLanguage })}
           </button>
           <button className={styles.btns} onClick={handlePriceHigher}>
-            Highest Price
+          {t("order-filter.highest-price", { lng: currentLanguage })}
           </button>
           <button className={styles.btns} onClick={handlePriceLower}>
-            Lowest Price
+          {t("order-filter.lowest-price", { lng: currentLanguage })}
           </button>
         </div>
       </div>
